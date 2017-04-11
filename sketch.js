b53ef80c27737647d4c5f594ceb75b545f8b685a
prusche <prusche@gmail.com>
2017-04-11 04:28:13 -0700

    added grid size selector

--- sketch.js
+++ sketch.js
@@ -1,6 +1,39 @@
 $(document).ready(function(){
 
 });
-$('td').hover(function() {
-   $(this).css('background', 'black');
+
+
+  // $('.boxes').hover(function(){
+  //   //$(this).css('background', 'green');
+  //   $(this).addClass('highlight');
+  // });
+
+  function showGrid(){
+    var size = $('#gridNum').val();
+    if (size < 2 || size > 64 || isNaN(size) ){
+      $('.box').empty();
+      $('.box').append("Invalid entry. Try again!");
+    } else {
+      makeTable(size);
+    }
+  }
+
+  function makeTable(size){
+    var dim = 600/size;
+    $('.box').empty();
+    for (var i = 0; i < size*size; i++) {
+     $('.box').append('<div class="boxes"></div>');
+     }
+    $('.boxes').css('width', dim);
+    $('.boxes').css('height', dim);
+    $('.boxes').hover(function(){
+      $(this).css('background', 'black');
+    });
+
+    //$('.boxes').css('background', 'pink');
+  }
+  makeTable(16);
+
+  $('.boxes').hover(function(){
+    $(this).css('background', 'black');
   });
